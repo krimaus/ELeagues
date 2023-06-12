ca679cad4ba9b95c9980291c891c599af294d456
matixoxo <m.mati111@wp.pl>
2023-06-12 13:23:29 +0200

    ciąg dalszy frontu, próba połączenia z bazą (nieudana)

--- ELeagues/LogOn.xaml.cs
+++ ELeagues/LogOn.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +23,40 @@ namespace ELeagues
     /// </summary>
     public partial class LogOn : Page
     {
+        public void save(object sender, RoutedEventArgs e)
+        {
+            string email = "";
+            string password = "";
+            string sec_password = "";
+            var conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["Conn"].ConnectionString);
+
+            try
+            {
+                conn.Open();
+
+                email = e_mail.Text.ToString();
+                password = pass.Text.ToString();
+                sec_password = sec_pass.Text.ToString();
+
+                string commandString = "SELECT max(id) FROM Uzyt;";
+                using (var cmd = new NpgsqlCommand(commandString, conn)) 
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Trace.WriteLine(reader.GetString(0));
+                    }
+                }
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Coś poszło nie tak");
+                conn.Close();
+            }
+        }
+
         public LogOn()
         {
             InitializeComponent();
