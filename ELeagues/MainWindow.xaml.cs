fe0305fd74f0824eb4ac4a9a7aad9f965dc39e68
krimaus <krimaus01@gmail.com>
2023-06-22 13:15:30 +0200

    Beginning of .net 6.0 migration

--- ELeagues/MainWindow.xaml.cs
+++ ELeagues/MainWindow.xaml.cs
@@ -1,8 +1,5 @@
-﻿using Npgsql;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,58 +20,9 @@ namespace ELeagues
     /// </summary>
     public partial class MainWindow : Window
     {
-
         public MainWindow()
         {
-            
-
-            /*var conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["Conn"].ConnectionString);
-            conn.Open();
-
-            using (var cmd = new NpgsqlCommand("SELECT version();", conn))
-            using (var reader = cmd.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    Trace.WriteLine(reader.GetString(0));
-                }
-            }*/
-
-            //zamiast bawic sie z licznikami w sql robic to po stronie aplikacji???
-            //wykrywac pierwsze nieuzyte id i przypisywac
-            /*
-            string id = "5";
-            string username = "monalisaEnjoyer";
-            string table = "eleagues";
-            string commandText = "INSERT INTO " + table + " VALUES (\'" + username + "\', " + id + ");";
-
-            using (var cmd = new NpgsqlCommand(commandText, conn))
-            {
-                try
-                {
-                    cmd.ExecuteNonQuery();
-                }
-                catch
-                {
-                    Trace.WriteLine("FUBAR sql command");
-                    Close();
-                }
-            }
-            */
-            /*var dataSource = NpgsqlDataSource.Create(ConfigurationManager.ConnectionStrings["Conn"].ConnectionString);
-
-            using (var cmd = dataSource.CreateCommand("SELECT * FROM eleagues"))
-            using (var reader = cmd.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    Console.WriteLine(reader.GetString(0));
-                }
-            }*/
-
             InitializeComponent();
         }
-
-        
     }
 }
