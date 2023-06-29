da367211bef96ed285c7a49f17c505228ccff253
krimaus <krimaus01@gmail.com>
2023-06-29 07:48:00 +0200

    Chodzi ale nic nie wyświetla

---
+++ ELeagues/LogIn.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ELeagues
+{
+    /// <summary>
+    /// Logika interakcji dla klasy LogIn.xaml
+    /// </summary>
+    public partial class LogIn : Page
+    {
+        public async void save(object sender, RoutedEventArgs e)
+        {
+            string email = "";
+            string pass = "";
+            var conn = "--";
+            //var dataSource = NpgsqlDataSource.Create(conn);
+            // otwarcie połączenia z bazą
+            try
+            {
+                email = e_mail.Text.ToString();
+                pass = password.Text.ToString();
+
+                if (email != "" && pass != "")
+                {
+                    //zapytania do bazy
+                    this.NavigationService.Navigate(new UserPage());
+                    //zamknięcie połączenia
+                }
+                else
+                {
+                    MessageBox.Show("Nieprawidłowe dane, prosze wprowadź ponownie");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Coś poszło nie tak");
+                //zamknięcie połączenia
+            }
+        }
+
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ELeagues/NavigationPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ELeagues
+{
+    /// <summary>
+    /// Logika interakcji dla klasy NavigationPage.xaml
+    /// </summary>
+    public partial class NavigationPage : Page
+    {
+        public NavigationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+            var ClickedBtn = e.OriginalSource as NavButton;
+            NavigationService.Navigate(ClickedBtn.NavUri);
+        }
+    }
+}
