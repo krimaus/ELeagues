b098b9718031441de275884682ca32f34cabe00c
krimaus <krimaus01@gmail.com>
2023-06-29 14:42:06 +0200

    Udany Reverse Engineering bazy danych

---
+++ ELeagues/Models/Ligi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ELeagues.Models;
+
+public partial class Ligi
+{
+    public int Idligi { get; set; }
+
+    public int Idwlasciciela { get; set; }
+
+    public virtual Uzytkownicy IdwlascicielaNavigation { get; set; } = null!;
+
+    public virtual ICollection<Turnieje> Turniejes { get; set; } = new List<Turnieje>();
+}
